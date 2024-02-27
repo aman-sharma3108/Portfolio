5e260b09a49b6ed08c907f83ef40ba450cd355a5
aman-sharma3108 <amansh.3108@gmail.com>
2024-02-27 11:37:53 +1100

    new update experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import Pdf from "../Swinburne.zip";
 export default function Experience(){
     return(
         <section className="relative md:py-24 py-16 bg-gray-50 dark:bg-slate-800" id="experience">
@@ -23,6 +25,24 @@ export default function Experience(){
                                 <div className="ltr:float-left rtl:float-right text-start md:ms-8 mt-6 md:mt-0">
                                     <h5 className="title mb-1 font-semibold">Developer</h5>
                                     <p className="mt-3 mb-0 text-slate-400 text-[15px]">The generated injected humour, or non-characteristic words etc. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="mt-12 ms-8 md:ms-0 relative after:content-[''] after:absolute after:top-[9px] after:rounded-full after:z-10 after:w-2.5 after:h-2.5 after:bg-amber-500 md:after:mx-auto ltr:md:after:right-0 ltr:md:after:left-0 ltr:after:-left-9 rtl:md:after:left-0 rtl:md:after:right-0 rtl:after:-right-9 before:content-[''] before:absolute md:before:mx-auto ltr:md:before:right-0 ltr:md:before:left-0 ltr:before:-left-11 rtl:md:before:left-0 rtl:md:before:right-0 rtl:before:-right-11 before:rounded-full before:z-10 before:border-2 before:border-dashed before:border-gray-200 dark:before:border-gray-700 before:top-0 before:w-7 before:h-7 before:bg-white dark:before:bg-slate-900">
+                            <div className="grid md:grid-cols-2">
+                                <div className="text-start md:ms-8 relative md:order-2">
+                                    <img src="/images/logos/swin.png" className=" md:me-auto" alt=""/>
+                                    <h5 className="my-2 font-semibold text-lg">Swinburne University Of Technology</h5>
+                                    <h6 className="text-sm mb-0">March 2019 - Nov 2023</h6>
+                                </div>
+
+                                <div className="ltr:float-left rtl:float-right md:text-end md:me-8 mt-6 md:mt-0 md:order-1">
+                                    <h5 className="title mb-1 font-semibold">Bachelor of Engineering(Honours)(Professional)</h5>
+                                    <p className="mt-3 mb-0 text-slate-400 text-[15px]">Majored in Software with a professional internship of 1 year at Cider House ICT Pty Ltd.
+                                    </p>
+                                    <Link target="_blank" to={Pdf} className="btn bg-amber-500/10 hover:bg-amber-500 border-amber-500/10 hover:border-amber-500 text-amber-500 hover:text-white rounded-md ms-1" >Transcript and Relevant Documents.</Link>
+
                                 </div>
                             </div>
                         </div>
